9cef9bf14934aae49ba7d801f8c72bffe5224f15
Elvis020 <spaceyel3@gmail.com>
2021-11-13 18:07:01 +0000

    Solved the rest of the questions :)

--- src/main/scala/org/example/movies/MovieApp.scala
+++ src/main/scala/org/example/movies/MovieApp.scala
@@ -36,7 +36,7 @@ object MovieApp extends App {
       s"${countFrom1987} movies"
     }
   )
-  
+
   printResult(
     "How many of the movies were released in 1987?",
     answer = {
@@ -45,4 +45,76 @@ object MovieApp extends App {
     }
   )
 
+  printResult(
+    "How many of the movies don't have release dates",
+    answer = {
+      val countMoviesWithoutReleaseDates = movies.map(_.releaseDate).count(_ == None)
+      s"${countMoviesWithoutReleaseDates} movies"
+    }
+  )
+  printResult(
+    "What are the top 5 movies per vote average and count?",
+    answer = {
+      val top5PerVoteAverage = movies.filter(_.voteCount > 50).sortBy(movie => (-movie.voteAverage, -movie.voteCount)).take(5)
+            top5PerVoteAverage.map{
+              movie => s"[AVG: ${movie.voteAverage}, COUNT:${movie.voteCount}] -> ${movie.title}\n"}
+          }.toString()
+  )
+
+  printResult(
+    "What are the top 5 movies per popularity?",
+    answer = {
+      val top5PerPopularity = movies.sortBy(mov => -mov.popularity.getOrElse(0f)).take(5)
+      top5PerPopularity.map{
+        movie => s"[POPULARITY : ${movie.popularity.getOrElse(unknown)} - ${movie.title}]\n"
+      }.toString()
+    }
+  )
+  printResult(
+    "What are the top 5 non-english movies per popularity?",
+    answer = {
+      val top5NonEng = movies.filterNot(_.originalLanguage == "en").sortBy(p => -(p.popularity.getOrElse(0f))).take(5)
+      top5NonEng.map{
+        movie => s"[POPULARITY : ${movie.popularity.getOrElse(unknown)} - ${movie.title}]\n"
+      }.toString()
+    }
+  )
+  printResult(
+    "5 non-english movies?",
+    answer = {
+      val top5NonEng = movies.filterNot(_.originalLanguage == "en").take(5)
+      top5NonEng.map{
+        movie => s"LANG:${movie.originalLanguage}, RELEASE_DATE:${movie.releaseDate.getOrElse(unknown)} ${movie.title}(${movie.originalTitle})]\n"
+      }.toString()
+    }
+  )
+  printResult(
+    "How many Italian movies are there?",
+    answer = {
+      val italianMovies = movies.count(_.originalLanguage == "it")
+      s"There are ${italianMovies} italian movies"
+    }
+  )
+  printResult(
+    "How many German movies are there?",
+    answer = {
+      val germanMovies = movies.count(_.originalLanguage == "de")
+      s"There are ${germanMovies} german movies"
+    }
+  )
+  printResult(
+    "Which film made the most profit?",
+    answer = {
+      val filmWithMostProfit = movies.sortBy(movie => -(movie.revenue - movie.budget)).head
+      val formatter = java.text.NumberFormat.getInstance()
+      s"${filmWithMostProfit.title} made the most profit of $$${formatter.format(filmWithMostProfit.revenue - filmWithMostProfit.budget)}"
+    }
+  )
+  printResult(
+    "What are the genres of movies available in this dataset",
+    answer = {
+      val listOfGenres = movies.flatMap(_.genres).map(_.name).distinct
+      s"List of genres include: ${listOfGenres}"
+    }
+  )
 }
--- src/main/scala/org/example/movies/PrintResultHelpers.scala
+++ src/main/scala/org/example/movies/PrintResultHelpers.scala
@@ -12,7 +12,7 @@ package org.example.movies
  */
 object PrintResultHelpers{
   def printResult(question:String,answer:String):Unit = printResult(question,answers=List(answer))
-  def printResult(question:String,answer:Option[String]):Unit = printResult(question,answer.toList)
+  def printResult(question:String,answer:Option[String]):Unit = printResult(question,answers=answer.toList)
   def printResult(question:String,answers:List[String]):Unit = {
     println()
     println("==============")
